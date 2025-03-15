802f1be3b0be9f546e310d0a81763e34c041759b
MaximB0nd <sersim11@yandex.ru>
2025-03-15 13:59:28 +0300

    Modif Enums

--- firstLesson/Enums.swift
+++ firstLesson/Enums.swift
@@ -7,14 +7,52 @@
 
 import Foundation
 
-enum MoneyType {
+enum MoneyType: String {
     case ruble
     case dollar
+    case euro
 }
 
+enum workingMode: Int {
+    case office = 6
+    case remote
+    
+    init?(a: Int){
+        guard a > 0 else {
+            return nil
+        }
+        self = .office
+    }
+}
+
+enum days{
+    case day (String, String)
+}
+
+
+
+
 func testEnums() {
-    var money: MoneyType = .ruble
+    var money: MoneyType = .dollar
     print(type(of:money))
+    
+    switch money {
+    case .ruble:
+        print("Рубль")
+    case .dollar:
+        print("Доллар")
+    case .euro:
+        print("Euro")
+        
+    }
+    var work = workingMode(a: -1)
+    print(workingMode.office.rawValue)
+    
+    var day = days.day("Понедельник", "Вторник")
+    switch day {
+    case .day(let first, let second):
+        print("\(first) \(second)")
+    }
 }
 
 
